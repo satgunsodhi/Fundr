97ff4479866ab8f7204c6a18372a9f8802dc0228
satgunsodhi <32atsatgun@gmail.com>
2025-04-13 17:58:42 +0530

    backend implementation

--- server.js
+++ server.js
@@ -1,30 +1,19 @@
 import express from "express";
-import dotenv from "dotenv";
 import path from "path";
-import {connectDB} from "./config/db.js";
-
-import productRoutes from "./routes/product.route.js";
-
-dotenv.config();
 
 const app = express();
 const PORT = Number(process.env.PORT) || 5000;
 
 const _dirname = path.resolve();
 
-app.use(express.json()); // allows us to accept JSON data in the req.body
+app.use(express.json());
 
-app.use("/api/products/", productRoutes);
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
 
-if(process.env.NODE_ENV ===  "production") {
-    app.use(express.static(path.join(_dirname, "/frontend/dist")));
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
-    })
-}
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+})
 
 app.listen(PORT, () => {
-    connectDB();
     console.log("Server Started at http://localhost:" + PORT);
 })
